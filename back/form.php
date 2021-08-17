455bf46f96a3614a11167837d04bf90c0b82c32a
FtohTarek <ftohtarek48@gmail.com>
2021-08-17 20:04:17 +0200

    change directory

--- back/form.php
+++ back/form.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     if ($hour > 0) {
         $calcSalary = new calcSalary($department, $hour);
         $salaryDetails = $calcSalary->Details();
-        include '../include/printResult.php';
+        include 'include/printResult.php';
     } else {
         echo "<div class='container'> <p class='text-danger'>Please Enter Valid Hour </p></div>";
     }
--- view/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include "../include/header.php";
-include "../doc/function.php";
-include "../include/formDesign.php";
-include "../back/gbaSystem.php";
-include "../back/form.php";
-include "../include/footer.php";
